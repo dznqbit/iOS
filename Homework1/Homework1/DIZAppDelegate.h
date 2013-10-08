cdab494605f91b8b60cff898ce554b3746b5545b
Quinton Harris (dznqbit) <quinton.harris@gmail.com>
2013-10-08 13:04:54 -0400

    HW1 Init checkin

---
+++ Homework1/Homework1/DIZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DIZAppDelegate.h
+//  Homework1
+//
+//  Created by Quinton Harris on 2013.10.08.
+//  Copyright (c) 2013 Quinton Harris. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DIZAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Homework1/Homework1/DIZAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  DIZAppDelegate.m
+//  Homework1
+//
+//  Created by Quinton Harris on 2013.10.08.
+//  Copyright (c) 2013 Quinton Harris. All rights reserved.
+//
+
+#import "DIZAppDelegate.h"
+
+@implementation DIZAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
